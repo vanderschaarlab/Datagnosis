18443b8ca37b6b04c6693fb88a8cd61d21f5ad4a
robsdavis <robsdavis473@gmail.com>
2023-08-23 14:54:16 +0100

    Replace reqs

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 nbsphinx_execute = "never"
